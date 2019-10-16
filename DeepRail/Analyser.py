ddc5a15c193b67bfbafe96a8a046cee2d335e5ff
Unknown <adrien.lauwers@student.uclouvain.be>
2019-10-16 16:09:16 +0200

    Up

--- DeepRail/Analyser.py
+++ DeepRail/Analyser.py
@@ -10,8 +10,9 @@ if __name__ == "__main__":
     stride = 0.000314
     mid = stride/2
     lat = 50.7105615
-    lng = 4.38
+
     for i in range(10):
+        lng = 4.38
         for j in range(10):
             response = requests.get(
                     "https://maps.googleapis.com/maps/api/staticmap?center=" +
@@ -24,6 +25,7 @@ if __name__ == "__main__":
             img = Image.open(BytesIO(response.content))
             w, h = img.size
             margin = 20
-            img.crop((margin, margin, w - margin, h - margin)).save("dataset/analyse" + str(str(i)+str(j)).zfill(5) + ".png", "png")
+            img.crop((margin, margin, w - margin, h - margin)).save("dataset/Analyse/analyse" + str(str(i)+str(j)).zfill(5) + ".png", "png")
+            print(lat,lng)
             lng += stride
         lat +=stride
