fb1ea708639bace83a18309429604e0104754c71
Jonathan Samelson <jonathan@samelson.info>
2019-10-18 15:13:07 +0200

    Added cropping method

--- src/TilesMaker.py
+++ src/TilesMaker.py
@@ -2,21 +2,25 @@ import os
 from tqdm import tqdm
 from PIL import Image
 
-if __name__ == "__main__":
 
-    PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'dataset')
-    images_dir = os.path.join(PATH, 'images')
-
-    height = 256
-    width = 256
-
+def crop_orig(pixel_crop, images_dir):
+    print("Croping original files")
     for i in tqdm(os.listdir(images_dir)):
         img_path = os.path.join(images_dir, i)
-        
+        img = Image.open(img_path, "r")
+        w, h = img.size
+        im = img.crop((pixel_crop, pixel_crop, w - pixel_crop, h - pixel_crop))
+        im.save("../DeepRailDataset/DOTA/part2/cropped/" + i, "PNG")
+
+    return
+
+def make_tiles(width, height, images_dir):
+    print("Creating tiles")
+    for i in tqdm(os.listdir(images_dir)):
+        img_path = os.path.join(images_dir, i)
+        img = Image.open(img_path, "r")
         if os.path.isdir(img_path): 
             continue
-
-        img = Image.open(img_path, mode="r")
 
         orig_width, orig_height = img.size
         
@@ -29,15 +33,27 @@ if __name__ == "__main__":
             right = width
             while right <= orig_width:
                 im = img.crop((left, top, right, bottom))
-                im.save("dataset/images/tiles/" + str(i).replace(".png","") + "_" + str(cnt) + ".png", "PNG")
+                im.save("tiles/" + str(i).replace(".png","") + "_" + str(cnt) + ".png", "PNG")
                 left = right + 1
                 right += width
                 cnt += 1
 
             top = bottom + 1
-            bottom += height        
+            bottom += height
+    return
 
-        
+if __name__ == "__main__":
 
+    directory = os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+    tiles = os.path.join(directory, 'DeepRailDataset/DOTA')
+    images_dir = os.path.join(tiles, 'part2')
 
+    height = 256
+    width = 256
 
+    pixel_crop = 100
+
+    crop_orig(pixel_crop, images_dir)
+
+    # images_dir = os.path.join(images_dir, 'cropped')
+    # make_tiles(width, height, images_dir)
