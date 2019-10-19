4b79a5ebea8e5f036c22ada0313d74b693f99077
Jonathan Samelson <jonathan@samelson.info>
2019-10-19 11:00:49 +0200

    Setup git lfs and updated TilesMaker

--- src/TilesMaker.py
+++ src/TilesMaker.py
@@ -7,10 +7,12 @@ def crop_orig(pixel_crop, images_dir):
     print("Croping original files")
     for i in tqdm(os.listdir(images_dir)):
         img_path = os.path.join(images_dir, i)
+        if os.path.isdir(img_path): 
+            continue
         img = Image.open(img_path, "r")
         w, h = img.size
         im = img.crop((pixel_crop, pixel_crop, w - pixel_crop, h - pixel_crop))
-        im.save("../DeepRailDataset/DOTA/part2/cropped/" + i, "PNG")
+        im.save("../DeepRailDataset/DOTA/part3/cropped/" + i, "PNG")
 
     return
 
@@ -35,25 +37,25 @@ def make_tiles(width, height, images_dir):
                 im = img.crop((left, top, right, bottom))
                 im.save("tiles/" + str(i).replace(".png","") + "_" + str(cnt) + ".png", "PNG")
                 left = right + 1
-                right += width
+                right += (width + 1)
                 cnt += 1
 
             top = bottom + 1
-            bottom += height
+            bottom += (height + 1)
     return
 
 if __name__ == "__main__":
 
     directory = os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
     tiles = os.path.join(directory, 'DeepRailDataset/DOTA')
-    images_dir = os.path.join(tiles, 'part2')
+    images_dir = os.path.join(tiles, 'part3')
 
     height = 256
     width = 256
 
     pixel_crop = 100
 
-    crop_orig(pixel_crop, images_dir)
+    # crop_orig(pixel_crop, images_dir)
 
-    # images_dir = os.path.join(images_dir, 'cropped')
-    # make_tiles(width, height, images_dir)
+    images_dir = os.path.join(images_dir, 'cropped')
+    make_tiles(width, height, images_dir)
