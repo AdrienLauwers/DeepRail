3a7d164a79dd40b17a61c76347e4d31d6b5601ab
Jonathan Samelson <jonathan@samelson.info>
2019-10-25 00:18:23 +0200

    Cleaned code

--- src/Analyser.py
+++ src/Analyser.py
@@ -19,9 +19,9 @@ def get_api_key():
     return API_keys
 
 def get_next_coord(coord, dist):
-    K = 1852*60 # 1 mille marin  = 1852 m; à multiplier par 60 pour passer aux degrés.
+    K = 1852*60 # 1 nautical marin  = 1852 m; to multiply by 60 to get degrees
     delta = dist/K
-    return coord + delta # * cos(0) = 1
+    return coord + delta
 
 def meters_per_pixel_NS(lat, zoom): 
     return 156543.03392 * math.cos(math.radians(lat)) / 2**zoom
@@ -29,29 +29,11 @@ def meters_per_pixel_NS(lat, zoom):
 def meters_per_pixel_EW(zoom):
     return 156543.03392 / 2**zoom
 
-
-# if __name__ == "__main__":
-#     # lat = 50.0
-#     # lng = 90.0
-#     lat = 50.677627
-#     lng = 4.562762
-#     dist_NS = 256 * meters_per_pixel_NS(lat, 20)
-#     dist_EW = 256 * meters_per_pixel_EW(20)
-#     # print(dist)
-#     new_lat = get_next_lat(lat, dist_NS)
-#     new_long = get_next_long(lng, lat, dist_EW)
-#     print("-------------")
-#     print("new lat", new_lat, (new_lat-lat))
-#     print("new long", new_long, (new_long-lng))
-
-
 if __name__ == "__main__":
 
     API_keys = get_api_key()
     API_key = API_keys["GoogleMaps"]
 
-    # stride = 0.000314
-    # mid = stride/2
     lat = 50.7605474
     lng_ori = 4.4586163
 
@@ -75,6 +57,4 @@ if __name__ == "__main__":
             img.crop((0, 0, w - margin, h - margin)).save("../../DeepRailDataset/analyse/img7/test" + str(str(i)+str(j)).zfill(5) + ".png", "png")
             print(lat,lng)
             lng = get_next_coord(lng, dist_EW)
-            # lng += stride
-        # lat +=stride
         lat = get_next_coord(lat, dist_NS)
--- src/ResNet50_test_area.py
+++ src/ResNet50_test_area.py
@@ -22,29 +22,10 @@ resnet50_model.load_weights("../models/resnet50_epoch55.h5")
 print("Loaded model from disk")
  
 PATH = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-#test_dir = os.path.join(os.path.join(PATH, '../../DeepRailDataset'), 'test')
-#test_rail_dir = os.path.join(test_dir, 'rail')
-#test_nonrail_dir = os.path.join(test_dir, 'nonrail')
-
 test_analyse = os.path.join(os.path.join(os.path.join(PATH, 'DeepRailDataset'), 'analyse'), 'img7')
 
 test_images = []
 test_path = []
-
-# for i in tqdm(os.listdir(test_rail_dir)):
-# 	img_path = os.path.join(test_rail_dir, i)
-# 	test_path.append(img_path)
-# 	img = cv2.imread(img_path, cv2.IMREAD_COLOR)
-# 	img = cv2.resize(img, (224, 224))
-# 	test_images.append([np.array(img), [1, 0]])
-
-# for i in tqdm(os.listdir(test_nonrail_dir)):
-#     img_path = os.path.join(test_nonrail_dir, i)
-#     test_path.append(img_path)
-#     img = cv2.imread(img_path, cv2.IMREAD_COLOR)
-#     img = cv2.resize(img, (224, 224))
-#     test_images.append([np.array(img), [0, 1]])
 
 for i in tqdm(sorted(os.listdir(test_analyse))):
 	img_path = os.path.join(test_analyse, i)
@@ -62,10 +43,7 @@ test_label = np.array([i[1] for i in test_images])
 
 
 # evaluate loaded model on test data
-
 resnet50_model.compile(loss='binary_crossentropy', optimizer=keras.optimizers.Adam(lr=1e-3), metrics=['accuracy'])
-# score = resnet50_model.evaluate(test_img, test_label, verbose=1)
-# print("%s: %.2f%%" % (resnet50_model.metrics_names[1], score[1]*100))
 
 predicts = resnet50_model.predict(test_img, verbose=1).reshape((-1,))
 
@@ -76,8 +54,6 @@ for i in range(int(len(predicts)//2)):
 		print(test_path[i], "rail", predicts[i*2])
 	else:
 		print(test_path[i], "non-rail", predicts[i*2+1])
-    # if int(predicts[i*2]) != test_label[i][0]:
-    #     print (i, predicts[i], test_label[i], test_path[i])
 
 full_img = Image.new('RGB', (WIDTH * 10, HEIGHT * 10))
 tiles = map(cv2.imread, test_path)
