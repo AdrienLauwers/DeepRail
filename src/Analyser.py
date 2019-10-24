ecdcd690e14210179c45ad9797c3fbcc81864f53
Jonathan Samelson <jonathan@samelson.info>
2019-10-25 00:05:54 +0200

    Fixed next coords in analyser

--- src/Analyser.py
+++ src/Analyser.py
@@ -18,15 +18,45 @@ def get_api_key():
 
     return API_keys
 
+def get_next_coord(coord, dist):
+    K = 1852*60 # 1 mille marin  = 1852 m; à multiplier par 60 pour passer aux degrés.
+    delta = dist/K
+    return coord + delta # * cos(0) = 1
+
+def meters_per_pixel_NS(lat, zoom): 
+    return 156543.03392 * math.cos(math.radians(lat)) / 2**zoom
+
+def meters_per_pixel_EW(zoom):
+    return 156543.03392 / 2**zoom
+
+
+# if __name__ == "__main__":
+#     # lat = 50.0
+#     # lng = 90.0
+#     lat = 50.677627
+#     lng = 4.562762
+#     dist_NS = 256 * meters_per_pixel_NS(lat, 20)
+#     dist_EW = 256 * meters_per_pixel_EW(20)
+#     # print(dist)
+#     new_lat = get_next_lat(lat, dist_NS)
+#     new_long = get_next_long(lng, lat, dist_EW)
+#     print("-------------")
+#     print("new lat", new_lat, (new_lat-lat))
+#     print("new long", new_long, (new_long-lng))
+
+
 if __name__ == "__main__":
 
     API_keys = get_api_key()
     API_key = API_keys["GoogleMaps"]
 
-    stride = 0.000314
-    mid = stride/2
-    lat = 50.6906239
-    lng_ori = 4.5731336
+    # stride = 0.000314
+    # mid = stride/2
+    lat = 50.7605474
+    lng_ori = 4.4586163
+
+    dist_NS = 256 * meters_per_pixel_NS(lat, 20)
+    dist_EW = 256 * meters_per_pixel_EW(20)
 
     for i in range(10):
         lng = lng_ori
@@ -42,7 +72,9 @@ if __name__ == "__main__":
             img = Image.open(BytesIO(response.content))
             w, h = img.size
             margin = 20
-            img.crop((margin, margin, w - margin, h - margin)).save("../../DeepRailDataset/analyse/img4/limal" + str(str(i)+str(j)).zfill(5) + ".png", "png")
+            img.crop((0, 0, w - margin, h - margin)).save("../../DeepRailDataset/analyse/img7/test" + str(str(i)+str(j)).zfill(5) + ".png", "png")
             print(lat,lng)
-            lng += stride
-        lat +=stride
+            lng = get_next_coord(lng, dist_EW)
+            # lng += stride
+        # lat +=stride
+        lat = get_next_coord(lat, dist_NS)
--- src/ResNet50_test_area.py
+++ src/ResNet50_test_area.py
@@ -7,10 +7,10 @@ import os
 import cv2
 import matplotlib.pyplot as plt
 from tqdm import tqdm
-from PIL import Image, ImageDraw, ImageFont
+from PIL import Image
 
-WIDTH = 256
-HEIGHT = 256
+WIDTH = 258
+HEIGHT = 258
 
 # load json and create model
 json_file = open('../models/resnet50_epoch55.json', 'r')
@@ -27,8 +27,8 @@ PATH = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)
 #test_rail_dir = os.path.join(test_dir, 'rail')
 #test_nonrail_dir = os.path.join(test_dir, 'nonrail')
 
-test_analyse = os.path.join(os.path.join(os.path.join(PATH, 'DeepRailDataset'), 'analyse'), 'img5')
-print(test_analyse)
+test_analyse = os.path.join(os.path.join(os.path.join(PATH, 'DeepRailDataset'), 'analyse'), 'img7')
+
 test_images = []
 test_path = []
 
@@ -48,10 +48,10 @@ test_path = []
 
 for i in tqdm(sorted(os.listdir(test_analyse))):
 	img_path = os.path.join(test_analyse, i)
-	test_path.append(img_path)
 	img = cv2.imread(img_path, cv2.IMREAD_COLOR)
 	if img is None:
 		continue
+	test_path.append(img_path)
 	img = cv2.resize(img, (224, 224))
 	test_images.append([np.array(img), [0, 1]])
 
@@ -80,26 +80,29 @@ for i in range(int(len(predicts)//2)):
     #     print (i, predicts[i], test_label[i], test_path[i])
 
 full_img = Image.new('RGB', (WIDTH * 10, HEIGHT * 10))
-tiles = map(Image.open, test_path)
+tiles = map(cv2.imread, test_path)
 
 cnt = 0
 x_offset = 0
-y_offset = HEIGHT * 10
+y_offset = HEIGHT * 9
 
+print(len(test_path))
 for img in tiles:
+  print(test_path[cnt])
   if (predicts[cnt*2] > predicts[cnt*2+1]):
     overlay = img.copy()
-    cv2.rectangle(overlay, (0, 0), (236,236), (0, 0, 200), -1)
+    cv2.rectangle(overlay, (0, 0), (256, 256), (0, 0, 200), -1)
     alpha = 0.15
     image_new = cv2.addWeighted(overlay, alpha, img, 1 - alpha, 0)
-    texted_image =cv2.putText(img=np.copy(image_new), text=str("%.2f" % predicts[cnt*2]), org=(0,30),fontFace=2, fontScale=1.5, color=(255,0,0), thickness=2)
+    texted_image =cv2.putText(img=np.copy(image_new), text=str("%.2f" % predicts[cnt*2]), org=(10,40),fontFace=2, fontScale=1.5, color=(255,0,0), thickness=2)
     full_img.paste( Image.fromarray(texted_image), (x_offset, y_offset))
   else:
     full_img.paste( Image.fromarray(img), (x_offset, y_offset))
   x_offset += WIDTH
+
   cnt += 1
   if (cnt % 10 == 0):
     x_offset = 0
     y_offset -= HEIGHT
 
-full_img.save("../../DeepRailDataset/analyse/img5/result.png", "PNG")
+full_img.save("../../DeepRailDataset/analyse/img7/result/result.png", "PNG")
